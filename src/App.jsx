b21603f1a4d0085941c3c11e8dece6d7d4dad0f9
Clara-Grigolon <29091@raphaeldisanto.com.br>
2026-04-14 12:31:51 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+import logo from './assets/lego-logo.webp'
+
+export default function App() {
+
+  return (
+    <>
+      <main>
+        <div className='logo'>
+          <img src={logo} alt="" />
+        </div>
+
+        <div className='container'>
+          <h1>Fazer Login</h1>
+
+          <p>Usuário:</p>
+          <input type="text" placeholder='Insira seu nome de usuário'/>
+
+          <p>Senha:</p>
+          <input type="password" placeholder='Insira sua senha'/>
+
+          <button>Entrar</button>
+
+          <a href="#">Esqueci minha senha</a>
+        </div>
+
+      </main>
+    </>
+  )
+}
